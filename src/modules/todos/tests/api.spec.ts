712fdc3ba79c5608f15fbb39fd6e009951b5833e
Vladimir <v.yancharuk@gmail.com>
2024-11-25 00:12:34 +0100

    add new todo unit tests

--- src/modules/todos/tests/api.spec.ts
+++ src/modules/todos/tests/api.spec.ts
@@ -7,6 +7,24 @@ import { addHeaders } from '../../../tests/utils';
 describe('Todos API', () => {
   const request = supertest(app);
 
+  let existingTodoUuid;
+
+  beforeAll(async () => {
+    const { ADMIN_JWT_TOKEN: jwtToken } = process.env;
+
+    // Fetch existing todos
+    const response = await addHeaders(request.get('/api/todos/my'), jwtToken);
+
+    expect(response.status).toBe(HTTP_STATUS.OK);
+
+    const todos = response.body.result;
+    expect(Array.isArray(todos)).toBe(true);
+    expect(todos.length).toBeGreaterThan(0);
+
+    // Store the UUID of the first todo
+    existingTodoUuid = todos[0].id;
+  });
+
   it('Should correctly load user todos', async () => {
     const { ADMIN_JWT_TOKEN: jwtToken } = process.env;
 
@@ -32,57 +50,59 @@ describe('Todos API', () => {
 
     const response = await addHeaders(
       request.post('/api/todos').send({
-        content: 'Complete tasks',
-        meta: {
-          expires_at: new Date(2025, 1, 1),
-        },
+        content: 'Complete test tasks',
+        fileSrc: 'https://s3.test.com/todo_logo.png',
+        expires_at: new Date(2025, 1, 1),
       }),
       jwtToken
     );
 
     expect(response.status).toBe(HTTP_STATUS.OK);
     expect(response.body.result).toEqual(expect.anything());
+    expect(response.body.result.content).toEqual('Complete test tasks');
+    expect(response.body.result.fileSrc).toEqual('https://s3.test.com/todo_logo.png');
+  });
+
+
+  it('Should NOT allow add new todo with invalid input', async () => {
+    const { ADMIN_JWT_TOKEN: jwtToken } = process.env;
+
+    const response = await addHeaders(
+      request.post('/api/todos').send({
+        // send 1 symbol
+        content: 'C',
+      }),
+      jwtToken
+    );
+
+    expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
   });
 
   it('Should correctly update existing todo', async () => {
     const { ADMIN_JWT_TOKEN: jwtToken } = process.env;
 
-    const todosRes = await addHeaders(request.get('/api/todos/my'), jwtToken);
-
-    expect(todosRes.status).toBe(HTTP_STATUS.OK);
-    expect(todosRes.body.result).toEqual(expect.anything());
-
-    const id = todosRes.body.result[0].id;
-
     const response = await addHeaders(
-      request.put(`/api/todos/${id}`).send({
+      request.put(`/api/todos/${existingTodoUuid}`).send({
         content: 'Complete tasks again',
+        fileSrc: 'https://s3.test.com/todo_new_logo.png',
       }),
       jwtToken
     );
 
     expect(response.status).toBe(HTTP_STATUS.OK);
     expect(response.body.result).toEqual(expect.anything());
+    expect(response.body.result.content).toEqual('Complete tasks again');
+    expect(response.body.result.fileSrc).toEqual('https://s3.test.com/todo_new_logo.png');
   });
 
   it('Should correctly delete existing todo', async () => {
     const { ADMIN_JWT_TOKEN: jwtToken } = process.env;
 
-    const todosRes = await addHeaders(request.get('/api/todos/my'), jwtToken);
-
-    expect(todosRes.status).toBe(HTTP_STATUS.OK);
-    expect(todosRes.body.result).toEqual(expect.anything());
-
-    const allTodos = todosRes.body.result;
-
-    const id = allTodos[allTodos.length - 1].id;
-
     const response = await addHeaders(
-      request.delete(`/api/todos/${id}`),
+      request.delete(`/api/todos/${existingTodoUuid}`),
       jwtToken
     );
 
     expect(response.status).toBe(HTTP_STATUS.OK);
-    expect(response.body.result).toEqual(expect.anything());
   });
 });
